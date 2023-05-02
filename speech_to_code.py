08f2d38a3021d59c92682fefac1d5a4216055cab
S.Dhanush <118609604+DhanushSundharam@users.noreply.github.com>
2023-05-02 23:40:21 +0530

    Create speech_to_code.py

---
+++ speech_to_code.py
@@ -0,0 +1,33 @@
+import  openai
+from openai import Completion
+import time
+import speech_recognition as sr
+openai.api_key = "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"
+def speechtotext():
+        r=sr.Recognizer()
+        with sr.Microphone() as source:
+                    r.adjust_for_ambient_noise(source)
+                    print("Please say Something")
+                    audio=r.listen(source)
+                    z=r.recognize_google(audio)
+                    print(z)
+                    return(z)
+                    
+
+def generate_text():
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=speechtotext(),
+        max_tokens=2000,
+        top_p=1,
+        temperature=0,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response.choices[0].text
+
+
+generated_text = generate_text()
+print(generated_text)
+
+
